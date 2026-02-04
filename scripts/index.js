4dc26cdd1537fc32b7c151705ded397191ef1b60
João Lima <joaolima@Joaos-Air.Home>
2026-02-04 11:08:49 +0000

    adiciona interatividade ao botão 'Edit Profile'

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,80 @@
+const profileBtn = document.querySelector(".profile__add-button");
+
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeBtn = editPopup.querySelector(".popup__close");
+const editProfileForm = editPopup.querySelector("#edit-profile-form");
+const inputName = editProfileForm.querySelector(".popup__input_type_name");
+const inputDescription = editProfileForm.querySelector(".popup__input_type_description")
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+
+
+const openModal = (modal) => {
+  modal.classList.add("popup_is-opened");
+}
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_is-opened");
+}
+
+profileEditBtn.addEventListener("click", () => {
+  handleOpenEditModal();
+   
+   
+});
+
+closeBtn.addEventListener( "click", ()  => {
+  closeModal(editPopup);
+
+});
+
+
+const handleOpenEditModal = () => {
+
+  fillProfileForm();
+  openModal(editPopup);
+
+
+}
+
+const fillProfileForm = () => {
+
+  const currentValues = {
+    title: profileTitle.textContent,
+    description: profileDescription.textContent
+  };
+
+  inputName.value = currentValues.title;
+  inputDescription.value = currentValues.description;
+  
+};
+
+const handleProfileFormSubmit = (evt) => {
+
+  evt.preventDefault();
+
+
+
+  const updateProfile = () => {
+  
+   profileTitle.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+
+  }
+  updateProfile();
+
+  closeModal(editPopup);
+
+
+
+};
+
+
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
+
 const initialCards = [
   {
     name: "Vale de Yosemite",
@@ -27,4 +104,6 @@ const initialCards = [
 
 initialCards.forEach((card) => {
   console.log(card.name);
+  
 });
+
